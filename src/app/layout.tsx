bce173672c1956c163b7a10476039f0f146208a0
DeinRanking GmbH <office@dein-ranking.at>
2026-01-13 16:24:07 +0100

    Add SessionProvider to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import ClientProvider from "@/components/providers/ClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        {children}
+        <ClientProvider>{children}</ClientProvider>
         <Toaster />
       </body>
     </html>
